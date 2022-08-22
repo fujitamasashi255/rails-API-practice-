98a7be328fd8f2daf1594ea6e9b30bfa0e1ec321
Masashi Fujita <masashifujita536@gmail.com>
2022-08-22 18:46:04 +0900

    rails db:migrateを実行してarticles、commentsテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_22_090309) do
+ActiveRecord::Schema.define(version: 2022_08_22_091020) do
+
+  create_table "articles", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title", null: false
+    t.text "contents"
+    t.integer "status", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "article_id", null: false
+    t.integer "user_id", null: false
+    t.text "contents"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "social_profiles", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -31,5 +51,8 @@ ActiveRecord::Schema.define(version: 2022_08_22_090309) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "articles", "users"
+  add_foreign_key "comments", "articles"
+  add_foreign_key "comments", "users"
   add_foreign_key "social_profiles", "users"
 end
