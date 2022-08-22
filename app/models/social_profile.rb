d810df5a54901539de872c1b1158df5de13b4b43
Masashi Fujita <masashifujita536@gmail.com>
2022-08-22 18:04:58 +0900

    social_profilesテーブルとモデルを作成

---
+++ app/models/social_profile.rb
@@ -0,0 +1,5 @@
+class SocialProfile < ApplicationRecord
+  belongs_to :user
+  validates :provider, presence: true
+  validates :uid, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_22_085236) do
+ActiveRecord::Schema.define(version: 2022_08_22_090309) do
+
+  create_table "social_profiles", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "provider", null: false
+    t.string "uid", null: false
+    t.string "raw_info"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_social_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -21,4 +31,5 @@ ActiveRecord::Schema.define(version: 2022_08_22_085236) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "social_profiles", "users"
 end
