348530a7c1de58f7a64c2b9c73ea0f350393e206
Masashi Fujita <masashifujita536@gmail.com>
2022-08-25 02:03:30 +0900

    プルリクエスト時の指摘を修正

--- app/controllers/api/v1/authentications_controller.rb
+++ app/controllers/api/v1/authentications_controller.rb
@@ -1,12 +1,10 @@
 class Api::V1::AuthenticationsController < Api::V1::BaseController
   def create
     user = login(params[:email], params[:password])
-    if user
-      json_string = UserSerializer.new(user).serialized_json
-      render json: json_string
-    else
-      raise ActiveRecord::RecordNotFound
-    end
+    raise ActiveRecord::RecordNotFound unless user
+
+    json_string = UserSerializer.new(user).serialized_json
+    render json: json_string
   end
 
   private
