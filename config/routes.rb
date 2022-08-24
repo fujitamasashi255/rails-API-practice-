0ba4d4b75e359d2632ceb77a6717b53feceaed41
Masashi Fujita <masashifujita536@gmail.com>
2022-08-25 01:09:38 +0900

    authenticationコントローラを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, format: 'json' do
     namespace :v1 do
       resources :articles, only: %i[index show]
+      post 'authentication' => 'authentications#create', as: :authentication
     end
   end
 end
