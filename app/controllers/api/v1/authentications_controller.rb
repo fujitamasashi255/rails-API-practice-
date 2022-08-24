461c1cb6f2b95207d49fbf290bed2fb80133f292
Masashi Fujita <masashifujita536@gmail.com>
2022-08-25 01:33:37 +0900

    authentication#createを作成

--- app/controllers/api/v1/authentications_controller.rb
+++ app/controllers/api/v1/authentications_controller.rb
@@ -1,4 +1,11 @@
-class Api::V1::AuthenticationsController < ApplicationController
+class Api::V1::AuthenticationsController < Api::V1::BaseController
   def create
+    user = login(params[:email], params[:password])
+    if user
+      json_string = UserSerializer.new(user).serialized_json
+      render json: json_string
+    else
+      raise ActiveRecord::RecordNotFound
+    end
   end
 end
