28d9aa70c9772c44472f32addfaf63e1ea23a42e
Masashi Fujita <masashifujita536@gmail.com>
2022-08-25 18:05:23 +0900

    コードをDRYに

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::RegistrationsController < Api::V1::BaseController
       json_string = UserSerializer.new(user).serialized_json
       render json: json_string
     else
-      render_error(message: 'Bad Request', status: 400, errors: user.errors.full_messages)
+      render_error(error_400_options, errors: user.errors.full_messages)
     end
   end
 
--- app/controllers/concerns/api/v1/error_handling.rb
+++ app/controllers/concerns/api/v1/error_handling.rb
@@ -13,24 +13,36 @@ module Api
       private
 
       def render_400(error)
-        render_error(message: 'Bad Request', status: 400, errors: error.message)
+        render_error(error_400_options, errors: error.message)
       end
 
       def render_404(error)
-        render_error(message: 'Record Not Found', status: 404, errors: error.message)
+        render_error(error_404_options, errors: error.message)
       end
 
       def render_500(error)
-        render_error(message: 'Internal Server Error', status: 500, errors: error.message)
+        render_error(error_500_options, errors: error.message)
       end
 
-      def render_error(message:, status:, errors:)
+      def render_error(error_options, errors:)
         response = {
-          message: message,
+          message: error_options[:message],
           errors: Array(errors)
         }
 
-        render json: response, status: status
+        render json: response, status: error_options[:status]
+      end
+
+      def error_400_options
+        { message: 'Bad Request', status: 400 }
+      end
+
+      def error_404_options
+        { message: 'Record Not Found', status: 404 }
+      end
+
+      def error_500_options
+        { message: 'Internal Server Error', status: 500 }
       end
     end
   end
