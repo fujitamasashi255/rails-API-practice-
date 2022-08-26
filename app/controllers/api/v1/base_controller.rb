14d3d4fa383c7dac4e6f0b5b47abceaf35632f6e
Masashi Fujita <masashifujita536@gmail.com>
2022-08-27 03:32:12 +0900

    テストエラーを修正

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -3,6 +3,7 @@
 module Api
   module V1
     class BaseController < ApplicationController
+      include Api::V1::ErrorHandling
       include ActionController::HttpAuthentication::Basic::ControllerMethods
       include ActionController::HttpAuthentication::Token::ControllerMethods
 
